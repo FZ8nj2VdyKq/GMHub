166e69193a41ae4ce0a1bf417392501fd0a5dcfe
GMlong <148448409+FZ8nj2VdyKq@users.noreply.github.com>
2024-03-09 19:11:30 +0700

    Add files via upload

---
+++ codes.lua
@@ -0,0 +1,29 @@
+local codes = {
+    "BigUpdateWednesday",
+    "BlamsSecretMiniUpdateMustBeSubbedToWork",
+    "evolvethis",
+    "ThankYouSoMuch45mVisits",
+    "Sub2Noclypso176k",
+    "Sub2KingLuffy",
+    "Sub2MayyjeeeOrCodeWontWork",
+    "D1SGUISED",
+    "Sub2BmGTormenter117onYT",
+    "Sub2CodeNex77k",
+    "NeelsTV",
+    "ALSUpdate2YutaHype",
+    "Real",
+    "PORTALS",
+    "SorryForDelayAndMaintenance",
+    "PortalBugFixes",
+    "TORMENTER10KSUBSLETSGOOOOOOO",
+    "Blams100kFavoritesCode",
+    "BackupMinatoMustBeSubbedtoTorToWork",
+    "BlamSpotsOP75MillVisitCodeMustBeSubbedToWork",
+    "shockssuperspecialultrasecretcodebecauseineedsubscribersmustbesubscribedtowork",
+    "Blams100kLikesCode",
+    "Update3", 
+    "Glitched",
+    "BLAMSPOTBUGFIXCODEMUSTBESUBBEDTOWORK!"
+}
+
+return codes
