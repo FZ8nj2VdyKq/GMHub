ef3f21436aa13e3e724e4169035aacbb99f87af6
GMlong <148448409+FZ8nj2VdyKq@users.noreply.github.com>
2024-03-09 19:50:23 +0700

    Update animedimesion.lua

--- animedimesion.lua
+++ animedimesion.lua
@@ -5,13 +5,7 @@ game:GetService("UserInputService").InputBegan:Connect(function(input)
         end
     end
 end)
-if game.PlaceId == 10598963932 then
-    print ("Cat Piece")
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/main/Cat%20piece"))()
-elseif game.PlaceId == 11448431235 then
-    print ("Rock Fruit")
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/main/Rock%20Fruit"))()
- elseif game.PlaceId == 6938803436 or 6990129309 or 6990133340 or 6990131029 or 7338881230 or 7274690025 or 6990129309 then
+ if game.PlaceId == 6938803436 or 6990129309 or 6990133340 or 6990131029 or 7338881230 or 7274690025 or 6990129309 then
     print ("Anime Dimensions")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/main/Anime%20Dimensions"))()
 else
