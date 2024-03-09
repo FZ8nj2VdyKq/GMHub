f16a1304929eb5e0dec5acdf5c84cef3e6e1924b
GMlong <148448409+FZ8nj2VdyKq@users.noreply.github.com>
2024-03-09 22:40:03 +0700

    Update codesALS.lua

--- codesALS.lua
+++ codesALS.lua
@@ -24,7 +24,6 @@ local codes = {
     "Update3", 
     "Glitched",
     "BLAMSPOTBUGFIXCODEMUSTBESUBBEDTOWORK!"
-    "shockssuperspecialultrasecretcodebecauseineedsubscribersmustbesubscribedtowork",
 }
 
 return codes
