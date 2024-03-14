bd7cf67f2172c236b7ea80d03ec74e627c0382ac
GMlong <148448409+FZ8nj2VdyKq@users.noreply.github.com>
2024-03-15 00:51:57 +0700

    Update AllGame.lua

--- AllGame.lua
+++ AllGame.lua
@@ -1,3 +1,11 @@
+game:GetService("UserInputService").InputBegan:Connect(function(input)
+    if input.KeyCode == Enum.KeyCode.LeftControl then
+        if game:GetService("CoreGui"):FindFirstChild("GMHub| "..game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name) then
+            game:GetService("CoreGui"):FindFirstChild("GMHub | "..game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name).Enabled = not game:GetService("CoreGui"):FindFirstChild("Muimi Hub | "..game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name).Enabled
+        end
+    end
+end)
+
 if game.PlaceId == 6938803436 or game.PlaceId == 6990129309 or game.PlaceId == 6990133340 or game.PlaceId == 6990131029 or game.PlaceId == 7338881230 or game.PlaceId == 7274690025 or game.PlaceId == 6990129309 then
     print ("Anime Dimensions")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Yanwanlnwza/SmellLikeHacker/main/Animedimensions.lua"))()
